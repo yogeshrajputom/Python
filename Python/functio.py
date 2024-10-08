d249ce854e8e032a0741bc32b517af638f44bb19
yogeshrajputom <153723012+yogeshrajputom@users.noreply.github.com>
2024-10-08 12:03:42 +0000

    8/10/2024

--- Python/functio.py
+++ Python/functio.py
@@ -1,7 +1,11 @@
+#Funtion Definition
 def fun():
+
     a = int(input("Enter no.1 "))
     b = int(input("Enter no.2 "))
     c = int(input("Enter no.3 "))
     average=(a+b+c)/2
     print (average)
+
+    #Function call 
 fun()
