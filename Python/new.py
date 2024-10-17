48e458dca029beaee36381532cfe879c10ed37df
yogeshrajputom <153723012+yogeshrajputom@users.noreply.github.com>
2024-10-17 10:49:06 +0000

    17/10/2024

---
+++ Python/new.py
@@ -0,0 +1,57 @@
+#print 1 to 10 netural number
+# i=0
+# while(i<=10):
+#     print(i)
+#     i+=1
+# row=int(input("Enter a Number:"))
+# print ('number pattern')
+
+# for i in range(1,row+1,1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print(" ")
+
+#print the sum of given number
+# n= int (input("enter your no:"))
+# b=sum(range(1,n+1))
+# print("Sum is :",b)
+
+
+#print the Table of given Number
+# b=int(input("enter your no:"))
+# for i in range(1,11,1):
+#     table=b*i
+#     print(table)
+
+
+# n = int(input("Enter a No. "))
+ 
+# for i in range (1,11,1):
+#     print(f"{n}X{i}={n*i}")
+
+# l=["Harry","Sohna","Sachin","Mohan"]
+# for name in l:
+#     if(name.startswith("S" )):
+#        print(name)
+
+
+# n = int (input("Enter the Number:"))
+# i=1
+# sum=0
+# while(i<=n):
+#     sum+=i
+#     i+=1
+#     print(sum)
+
+# b =int (input("Enter a Number:"))
+# fact=1
+# for i in range (1,b+1):
+#     fact=fact*i
+#     print(fact)
+
+
+# n=int (input("Enter a Number:"))
+# for i in range (1,n+1):
+#     print(" "*(n-i),end=" ")
+#     print("*"*(2*i-1), end=" ")
+#     print(" ")
